527ef267962d1888c545e917f8fdfeb480a5f68b
Melisa Ferreyra <mferreyra@soyhenry.com>
2022-12-16 12:29:36 -0300

    delete

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import NewAnimal from "./NewAnimal";
 import Tablita from "./Tablita"
 
 export default function Home() {
+    const [flag, setFlag] = useState(false);
+    const [modalIsOpen, setIsOpen] = useState(false);
     return (
         <>
             <h1>Animal Management</h1>
-            <NewAnimal/>
-
+            <NewAnimal setFlag={setFlag} modalIsOpen={modalIsOpen} setIsOpen={setIsOpen}/>
             <hr />
-            
-            <Tablita/>
+            <Tablita flag={flag} setIsOpen={setIsOpen}/>
         </>
     )
 }
--- client/src/utils.js
+++ client/src/utils.js
@@ -1,45 +1,45 @@
-import { Crud } from "./components/Funciones";
+//import  Crud  from "./components/Funciones";
 
-const { editAnimal, deleteAnimal } = Crud();
+//const { editAnimal, deleteAnimal } = Crud();
 
-export const COLUMNS = [
-  {
-    Header: "ID",
-    accessor: "id_number",
-  },
-  {
-    Header: "Type of Animal",
-    accessor: "typeOfAnimal",
-  },
-  {
-    Header: "Name of Potrero",
-    accessor: "nameOfPotrero",
-  },
-  {
-    Header: "Weight",
-    accessor: "weight",
-  },
-  {
-    Header: "Device Type",
-    accessor: "deviceType",
-  },
-  {
-    Header: "Device Number",
-    accessor: "deviceNumber",
-  },
-  {
-    Header: "Action",
-    accessor: (originalRow, rowIndex) => (
-      <div>
-        <button onClick={() => editAnimal(originalRow.id_number)}>Edit</button>
-        <button onClick={() => deleteAnimal(originalRow.id_number)}>
-          Delete
-        </button>
-      </div>
-    ),
-    id: "action",
-  },
-];
+// export const COLUMNS = [
+//   {
+//     Header: "ID",
+//     accessor: "_id",
+//   },
+//   {
+//     Header: "Type of Animal",
+//     accessor: "typeOfAnimal",
+//   },
+//   {
+//     Header: "Name of Potrero",
+//     accessor: "nameOfPotrero",
+//   },
+//   {
+//     Header: "Weight",
+//     accessor: "weight",
+//   },
+//   {
+//     Header: "Device Type",
+//     accessor: "deviceType",
+//   },
+//   {
+//     Header: "Device Number",
+//     accessor: "deviceNumber",
+//   },
+//   {
+//     Header: "Action",
+//     accessor: (originalRow, rowIndex) => (
+//       <div>
+//         <button onClick={() => editAnimal(originalRow._id)}>Edit</button>
+//         <button onClick={() => deleteAnimal(originalRow._id)}>
+//           Delete
+//         </button>
+//       </div>
+//     ),
+//     id: "action",
+//   },
+// ];
 
 const aux = [
   {
